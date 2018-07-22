3be17aba8a3dbac044d28ff02302d3da61e2d781
Lillian Byrne <lily.byrne.304@gmail.com>
2018-07-22 03:10:08 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -1,30 +1,48 @@
-# Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output. 
 
-# weather = {
-# 	:city => "New York",
-# 	:description => "Partly Cloudy",
-# 	:temperature_farenheit => 66.3
-# }
+weather = {
+ 	:city => "New York",
+ 	:description => "Partly Cloudy",
+ 	:temperature_farenheit => 66.3
+}
 
-# 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash. 
+
+
 def location(weather)
-	
+  return weather[:city]
+end
+location(weather)
+
+
+def description(weather)
+  return weather[:description]
+end 
+description(weather)
+
+
+def weather_report(weather)
+  return "The weather in #{location(weather)} is #{description(weather)}. The temperature is #{weather[:temperature_farenheit]} degrees Farenheit."
+end 
+
+
+def city_weather_info(weather)
+  weather.each do |key, value|
+  puts "The #{key} is #{value}."
 end
 
-# 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
-
-
-# 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
-
-
-#4. Write a method that takes in the weather hash as an argument. 
-# You'll want to use the each method to iterate over the hash and print out
-# `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
-
-
-# BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
-
-# Hint - how can we find the algorithm to convert Farenheit to Celsius?
 
 
 
+
+# #to convert farenheit to celcius you subtract 32 and multiple by 1.8
+
+# def temp_change(weather)
+#   farenheit = weather[temperature]
+#   farenheit.to_f 
+#   farenheit -= 32
+#   farenheit * 1.8 
+# end 
+
+
+# # BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
+
+# # Hint - how can we find the algorithm to convert Farenheit to Celsius?
