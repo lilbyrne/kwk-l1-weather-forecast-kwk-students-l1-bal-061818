0c72f3fe350f31324858eeae273c8a3df4bdab25
Lillian Byrne <lily.byrne.304@gmail.com>
2018-07-22 03:13:09 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -22,21 +22,3 @@ def city_weather_info(weather)
   weather.each do |key, value|
   puts "The #{key} is #{value}."
 end
-
-
-
-
-
-# #to convert farenheit to celcius you subtract 32 and multiple by 1.8
-
-# def temp_change(weather)
-#   farenheit = weather[temperature]
-#   farenheit.to_f 
-#   farenheit -= 32
-#   farenheit * 1.8 
-# end 
-
-
-# # BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
-
-# # Hint - how can we find the algorithm to convert Farenheit to Celsius?
